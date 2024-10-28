61417ca61e01bd798a141c07aafebf66d458d10d
djd <dananjayahbi@gmail.com>
2024-10-28 14:31:18 +0530

    data card

--- startup_app/app/(root)/page.tsx
+++ startup_app/app/(root)/page.tsx
@@ -1,9 +1,23 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{query?: string}>
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "John Doe" },
+      _id: 1,
+      description: "This is a description of the post",
+      image: "https://img.freepik.com/premium-photo/robot-mascot_113255-76373.jpg?semt=ais_hybrid",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
 
   return (
     <>
@@ -18,6 +32,22 @@ export default async function Home({ searchParams }: {
         </p>
 
         <SearchForm query={query}/>
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length>0?(
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ):(
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
